f4483f8dac6b87d7e1d3add876d30c8a12ad34dd
Arshjeet-Singh <88304260+Arshjeet-Singh@users.noreply.github.com>
2023-06-08 17:26:27 +0530

    Create Maximum Subarray Sum.cpp

---
+++ Maximum Subarray Sum.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+long long maxSubarraySum(int arr[], int n)
+{
+    long long ans=0,sum=0;
+    for(int i=0;i<n;i++)
+    {
+        sum+=arr[i];
+        if(sum<0)
+        {
+            sum=0;
+        }
+        ans=max(ans,sum);
+    }
+    return ans;
+    /*
+        Don't write main().
+        Don't read input, it is passed as function argument.    
+        No need to print anything.
+        Taking input and printing output is handled automatically.
+    */
+}
