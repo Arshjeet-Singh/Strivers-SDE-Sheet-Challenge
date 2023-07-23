f58a6af649fd6d627f5db6e15a378e60f3491079
Arshjeet-Singh <88304260+Arshjeet-Singh@users.noreply.github.com>
2023-07-23 13:57:21 +0530

    Update Count And Say.cpp

--- Count And Say.cpp
+++ Count And Say.cpp
@@ -5,31 +5,44 @@ string writeAsYouSpeak(int n)
 	{
 		return "1";
 	}
-	if(n==2)
+	string s=writeAsYouSpeak(n-1);
+	string s1="";
+	int cnt=0;
+	for(int i=0;i<s.length();i++)
 	{
-		return "11";
-	}
-	string s="11";
-	for(int i=3;i<=n;i++)
-	{
-		string t="";
-		s+='#';
-		int c=1;
-		for(int j=1;j<s.length();j++)
+		cnt++;
+		if(i==s.length()-1 || s[i]!=s[i+1])
 		{
-			if(s[j]!=s[j-1])
-			{
-				t+=to_string(c);
-				t+=s[j-1];
-				c=1;
-			}
-			else
-			{
-				c++;
-			}
+			s1+=to_string(cnt)+s[i];
+			cnt=0;
 		}
-		s=t;
 	}
-	return s;
+
+	// if(n==2)
+	// {
+	// 	return "11";
+	// }
+	// string s="11";
+	// for(int i=3;i<=n;i++)
+	// {
+	// 	string t="";
+	// 	s+='#';
+	// 	int c=1;
+	// 	for(int j=1;j<s.length();j++)
+	// 	{
+	// 		if(s[j]!=s[j-1])
+	// 		{
+	// 			t+=to_string(c);
+	// 			t+=s[j-1];
+	// 			c=1;
+	// 		}
+	// 		else
+	// 		{
+	// 			c++;
+	// 		}
+	// 	}
+	// 	s=t;
+	// }
+	return s1;
 	// Write your code here.	
 }
